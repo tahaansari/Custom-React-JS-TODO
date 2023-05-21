90a21c8ab8ee9576a928c763ecddfdac5b503afa
Taha Ansari <taha.m.ansari@gmail.com>
2023-05-21 11:07:57 +0530

    wip

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 export default function CreateTodo() {
+
+  const [todo,setTodo] = useState("")
+
+  const addTodo = ()=>{
+    alert("add todo called",todo)
+
+  }
+
   return (
     <div>
       <div className="d-flex ">
-        <input type="text" className="form-control" placeholder="Enter Something"/>
-        <button type="button" className="btn btn-success">Add</button>
+        <input type="text" value="" onChange={setTodo(todo)} className="form-control" placeholder="Enter Something"/>
+        <button type="button" onClick={addTodo} className="btn btn-success">Add</button>
       </div>
     </div>
   )
--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
 
-
 function ListTodo() {
 
-  const [list,setList] = useState("");  
+  const [todos,setTodos] = useState([])
+
+  const deleteTodo = ()=>{
+    alert('delete todo called')
+  }
 
   return (
     <div>
       <ul className="list-group">
-        <li className="list-group-item">
-          An item
-          <button type="button" className="btn-close float-end" aria-label="Close"></button>
-        </li>
+        {todos.map((todo,index)=>{
+          return(
+            <li key={index} className="list-group-item">
+              {todo}
+              <button type="button" onClick={deleteTodo} className="btn-close float-end" aria-label="Close"></button>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
